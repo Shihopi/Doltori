6a7bbd3dab5b08c432d0e6c565c1b83491d4d644
Shihopi <shiho_kitayama@femact.co.jp>
2016-04-17 21:52:11 +0900

    Removed rest line.

--- Doltori/FirstViewController.swift
+++ Doltori/FirstViewController.swift
@@ -14,7 +14,6 @@ class FirstViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("ハウ")
-        print("test commit")
     }
 
 }
