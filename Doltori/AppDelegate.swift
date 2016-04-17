0b27404f25f2848fe59ff000eac987409fed1161
北山 志帆 <shiho@shihonomac.local>
2016-04-17 21:42:13 +0900

    Initial Commit

---
+++ Doltori/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Doltori
+//
+//  Created by 北山 志帆 on 2016/04/17.
+//  Copyright © 2016年 Femact Inc. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
---
+++ Doltori/FirstViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FirstViewController.swift
+//  Doltori
+//
+//  Created by 北山 志帆 on 2016/04/17.
+//  Copyright © 2016年 Femact Inc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
